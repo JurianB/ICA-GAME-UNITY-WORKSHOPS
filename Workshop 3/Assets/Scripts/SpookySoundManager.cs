ad5f0ba402e5c379197dda2bb7e309ccdd71c78e
Tom Martens <tommar49@gmail.com>
2019-10-03 11:20:39 +0200

    Audio manager added

---
+++ Workshop 3/Assets/Scripts/SpookySoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpookySoundManager : MonoBehaviour
+{
+    public AudioSource Source;
+
+    [Range(0, 1)]
+    public float MinVolume;
+
+    [Range(0, 1)]
+    public float MaxVolume;
+
+    private GameObject[] _enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        Source.volume = MinVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
